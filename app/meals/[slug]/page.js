dee859280993cc6bed872ab8ecd41c4fd492c5ef
sai2963 <venkatsai0629@gmail.com>
2024-07-11 13:38:50 +0530

    Slug meal page added

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,46 @@
-const Restaurants =({params})=>{
-    return(
-        <>
-        <h1>Welcome to our restaurant</h1>
-        <p>{params.slug}</p>
-        </>
-    )
-}
-export default Restaurants
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+
+const Restaurants = ({ params }) => {
+  const meals = getMeal(params.slug);
+  console.log(meals.title);
+  return (
+    <>
+      <header className="m-20 flex px-4 py-8 gap-12  max-w-7xl">
+        <div className="relative w-96 h-80 object-cover rounded-[8px] [box-shadow:0_0_0.5rem_rgba(0,_0,_0,_0.5)] animate-[fade-slide-in-from-left_1s_ease-out_forwards]">
+          <Image className="" src={meals.image} alt={meals.title} fill />
+        </div>
+        <div className="p-2 pt-2 pb-0 text-[#ddd6cb] max-w-3xl animate-fade-slide-in-from-right">
+          <h1 className="m-0 text-8xl uppercase font-montserrat shadow-md">
+            {meals.title}
+          </h1>
+          <p className=" text-2xl text-[#cfa69b] italic">
+            by{" "}
+            <a
+              className="
+      bg-gradient-to-r from-[#f9572a] to-[#ff8a05] 
+      bg-clip-text text-transparent 
+      hover:bg-gradient-to-r hover:from-[#f9572a] hover:to-[#ff8a05] 
+      hover:bg-clip-text hover:text-transparent 
+      hover:shadow-[0_0_18px_rgba(248,190,42,0.8)]"
+              href={`mailto:${meals.creator_email}`}
+            >
+              {" "}
+              {meals.creator}
+            </a>
+          </p>
+          <p className=" text-xl py-5">{meals.summary}</p>
+        </div>
+      </header>
+      <main>
+      <p
+          className="text-[1.25rem] leading-normal bg-[#6e6464] text-[#13120f] rounded-[8px] [box-shadow:0_0_0.5rem_rgba(0,_0,_0,_0.5)] p-8 max-w-[60rem] mx-[auto] my-8 animate-[fade-slide-in-from-bottom_1s_ease-out_forwards]"
+          dangerouslySetInnerHTML={{
+            __html: meals.instructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
+};
+export default Restaurants;
---
+++ error.js
@@ -0,0 +1,12 @@
+'use client'
+const Error =()=>{
+    return(
+        <>
+        <main className="m-36 text-center">
+        <h1 className="text-3xl w-36" >An error occured</h1>
+        <p>Failed to fetch meal data. please try again later</p>
+        </main>
+        </>
+    )
+}
+export default Error;
