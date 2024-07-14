b5ba7b33e88f5527758d4592526f8ea9825bb48e
sai2963 <venkatsai0629@gmail.com>
2024-07-14 13:53:14 +0530

    Project style test 1

--- app/page.js
+++ app/page.js
@@ -5,20 +5,20 @@ export default function Home() {
   return (
     <>
       <div className="bg-gray-800">
-        <header className="text-white relative flex items-center justify-end ">
+        <header className="text-white flex flex-col items-center lg:flex-row lg:justify-end relative">
           {/* Left side image placeholder */}
-          <div className=" px-32 mx-10 flex justify-around" >
-          <div className="absolute y-10 inset-y-0 left-0 w-96 bg-cover bg-center h-80 m-36 ">
+          <div className="lg:w-1/2 flex justify-center lg:justify-start p-4">
+            <div className="w-80 h-80 lg:w-96 lg:h-96">
               <ImageSlider />
             </div>
+          </div>
 
-          <div className="container mx-auto text-center py-16 px-4 relative w-96 y-10 m-12">
-            
+          <div className="container mx-auto text-center py-16 px-4 relative lg:w-1/2 lg:py-32 lg:text-left">
             <div className="mb-8 mt-6">
-              <h1 className="text-5xl font-extrabold mb-4">
+              <h1 className="text-3xl lg:text-5xl font-extrabold mb-4">
                 NextLevel Food for NextLevel Foodies
               </h1>
-              <p className="text-xl text-gray-300">
+              <p className="text-lg lg:text-xl text-gray-300">
                 Taste & share food from all over the world.
               </p>
             </div>
@@ -37,13 +37,11 @@ export default function Home() {
               </Link>
             </div>
           </div>
-          </div>
-          
         </header>
         <main className="container mx-auto py-16 px-4">
           <section className="bg-gradient-to-r from-amber-500 via-amber-600 to-amber-700 shadow-xl rounded-lg p-8 mb-16">
             <h2 className="text-3xl font-bold mb-4">How it works</h2>
-            <p className="text-white-800 mb-4">
+            <p className="text-gray-800 mb-4">
               NextLevel Food is a platform for foodies to share their favorite
               recipes with the world. It&apos;s a place to discover new dishes,
               and to connect with other food lovers.
